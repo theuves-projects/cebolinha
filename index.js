ac468bc005449afeccf17a0637a30bab90334dc1
Matheus Alves <theuves@outlook.com>
2016-11-11 17:10:33 -0300

    Algumas alterações

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = function (str) {
-
     if (typeof str === 'string') {
-
         return str
             .replace(/r(?=(l|L))/g, 'u').replace(/R(?=(l|L))/g, 'U')
             .replace(/r+(?!\b)/g, 'l').replace(/R+(?!\b)/g, 'L')
