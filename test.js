7e270c4f84dec4731f7782bc915db026a58c70d9
Matheus Alves <theuves@outlook.com>
2018-02-13 19:33:29 -0300

    troca ava por jasmine

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-import test from 'ava';
-import cebolinha from './';
+"use strict";
 
-test('cebolinha', t => {
-    t.is(cebolinha('errado'), 'elado');
-    t.is(cebolinha('comer'), 'comer');
-    t.is(cebolinha('parlamento'), 'paulamento');
+const cebolinha = require("./");
+
+describe("cebolinha", () => {
+  it("deveria trocar o 'r' pelo 'l'", () => {
+    expect(cebolinha("prato")).toBe("plato");
+    expect(cebolinha("rato")).toBe("lato");
+  });
+  it("não deveria trocar o 'r' quando ele está no final", () => {
+    expect(cebolinha("trator")).toBe("trator");
+    expect(cebolinha("comer")).toBe("comer");
+  });
+  it("deveria trocar 'rl' por 'ul'", () => {
+    expect(cebolinha("carlos")).toBe("caulos");
+    expect(cebolinha("parlamento")).toBe("paulamento");
+  });
+  it("deveria obedecer a caixa alta", () => {
+    expect(cebolinha("PRATO")).toBe("PLATO");
+    expect(cebolinha("RATO")).toBE("LATO");
+  });
 });
+
