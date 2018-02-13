b67de3866d5bc40ec100e8cd04a5a121cf5c5ac5
Matheus Alves <theuves@outlook.com>
2018-02-13 19:19:40 -0300

    refaz código

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-'use strict';
+"use strict";
 
-module.exports = function (str) {
-    if (typeof str === 'string') {
-        return str
-            .replace(/r(?=(l|L))/g, 'u').replace(/R(?=(l|L))/g, 'U')
-            .replace(/r+(?!\b)/g, 'l').replace(/R+(?!\b)/g, 'L')
+function cebolinha(string) {
+  if (typeof string !== "string") return string;
 
-            .replace(/([^aeiouAEIOUà-üÀ-Ü]r\b)/g, function (xr) {
-                return xr.replace(/.$/, 'l');
-            })
-
-            .replace(/([^aeiouAEIOUà-üÀ-Ü]R\b)/g, function (xr) {
-                return xr.replace(/.$/, 'L');
-            });
-    }
+  return string
+    .replace(/r(?=(l|L))/g, "u")
+    .replace(/R(?=(l|L))/g, "U")
+    .replace(/r+(?!\b)/g, "l")
+    .replace(/R+(?!\b)/g, "L");
 };
+
+module.exports = cebolinha;
